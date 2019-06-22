3737d65a09120df426b190ce6e5bedcce5d70e12
Geoff <geoff.groos@empowerops.com>
2019-06-21 23:13:44 -0700

    well it builds

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "1.3.40"
+}
+group = "groostav"
+version = "0.3"
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+
+    implementation("com.google.guava:guava:19.0")
+    implementation("org.antlr:ST4:4.0.8")
+
+    testImplementation("junit:junit:4.11")
+    testImplementation("org.assertj:assertj-core:3.5.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.3.40")
+}
+repositories {
+    mavenCentral()
+}
